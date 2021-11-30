aebb8ea913db78512eff771dfbe97de173dd98c2
til_lsanchez <til_lsanchez@uolinc.com>
2021-11-29 23:50:23 -0300

    fix error

--- src/main/java/com/github/lionani07/appussers/client/VideoClient.java
+++ src/main/java/com/github/lionani07/appussers/client/VideoClient.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Component
 public class VideoClient {
